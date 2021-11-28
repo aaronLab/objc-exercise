88f1c81941df93c4333e32b7b45e8609ecbc34f8
Aaron Lee <aaronlab.net@gmail.com>
2021-11-28 18:23:10 +0900

    hello world

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Aaron Lee on 2021/11/28.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[]) {
+    
+    NSLog(@"Hell Objc!!!");
+    
+    return (0);
+    
+}
